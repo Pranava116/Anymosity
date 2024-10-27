79b10642564230619a02b2ce7d0e4f5057863e03
Pranava116 <pranavagrao116@gmail.com>
2024-10-27 12:56:08 +0530

    Client

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='main-navbar'>
+        <div className='navbar'>
+            <h2>****</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
