467b5af7420b607baad99fd4d3382d3eb7eb75fb
Pranava116 <pranavagrao116@gmail.com>
2024-12-01 22:46:19 +0530

    Can now recieve seemore page values

--- client/src/pages/SeeMore.jsx
+++ client/src/pages/SeeMore.jsx
@@ -3,11 +3,17 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 function SeeMore() {
   const [title , setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [date, setDate] = useState("")
+  const [venue, setVenue] = useState("")
     const {id} = useParams()
     async function getValues(){
       const response = await axios.put(`http://localhost:5000/main/${id}`)
       console.log(response)
       setTitle(response.data[0].title)
+      setDesc(response.data[0].desc)
+      setDate(response.data[0].date)
+      setVenue(response.data[0].venue)
     }
     useEffect(() => {
       getValues()
@@ -18,9 +24,9 @@ function SeeMore() {
     <div className='main-seemore-wrapper'>
       <div className='seemore-wrapper'>
         <h1 className='seemore-title'>{title}</h1>
-        <h3 className='seemore-desc'>dfngfjd</h3>
-        <h2 className='seemore-date'>2024-11-4</h2>
-        <h2 className='seemore-venue'>Bengaluru</h2>
+        <h3 className='seemore-desc'>{desc}</h3>
+        <h2 className='seemore-date'>{date}</h2>
+        <h2 className='seemore-venue'>{venue}</h2>
       </div>
     </div>
   )
